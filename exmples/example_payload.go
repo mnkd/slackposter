7da2548c193bac7799d03fbd756789aaded8cf0f
m-nakada <m-nakada@nekojarashi.com>
2017-01-13 10:39:50 +0900

    Add examples
    - example.go: post a message
    - example_payload.go: post a payload

---
+++ exmples/example_payload.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"time"
+
+	"github.com/m-nakada/slackposter"
+)
+
+func main() {
+	config := slackposter.Config{
+		"yourchannel",
+		":octocat:",
+		"GitHub | Status",
+		"https://hooks.slack.com/services/xxx/xxx/xxx",
+	}
+
+	slack := slackposter.NewSlack(config)
+
+	payload := slack.NewPayload()
+	payload.Mrkdwn = true
+
+	statusField := slackposter.Field{
+		Title: "Status",
+		Value: "Good",
+		Short: true,
+	}
+
+	dateString := time.Now().Format("2006-01-02 15:04")
+	dateField := slackposter.Field{
+		Title: "Date",
+		Value: dateString,
+		Short: true,
+	}
+
+	attachment := slackposter.Attachment{
+		Fallback: "GitHub Status: Good - https://status.github.com",
+		Text:     "<https://status.github.com/|GitHub Status> : *Good*",
+		Color:    "good",
+		Fields:   []slackposter.Field{statusField, dateField},
+		MrkdwnIn: []string{"text"},
+	}
+
+	payload.Attachments = []slackposter.Attachment{attachment}
+
+	slack.PostPayload(payload)
+}
